cae65dd39a72159c06a41563c967ffaf642206c0
Navdeeppal Singh <29735499+nps1ngh@users.noreply.github.com>
2022-10-16 23:03:59 +0200

    add Conv2d to nn

--- simple_autograd/nn/modules/__init__.py
+++ simple_autograd/nn/modules/__init__.py
@@ -2,6 +2,7 @@
 Code is inspired by PyTorch's API.
 """
 from .base import Module
+from .convolution import Conv2d
 from .linear import Linear
 from .utils import Sequential
 from .act import ReLU, LeakyReLU
